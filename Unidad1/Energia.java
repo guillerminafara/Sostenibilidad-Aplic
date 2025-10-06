52c3f7895ebc1bf31c5ec12af7a4c2f6bcdff758
guillermina <faramagui@hotmail.com>
2025-10-06 11:54:32 +0200

    tarea 5

--- Unidad1/Energia.java
+++ Unidad1/Energia.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -5,22 +6,63 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Energia{
-    public static void main(String[]args) throws IOException{
-        System.out.println("Hola");
-        String archivoCsv= "/home/marfarsan/Escritorio/daw/Sostenibilidad-Aplic/Unidad1/viviendas.csv";
-        List<String> lista= new ArrayList<>();
+public class Energia {
+
+    public static void main(String[] args) throws IOException {
+        mediana(args[0]);
+    }
+
+    static class Municipio {
+
+        String codigo;
+        String territorio;
+        Double valor;
+
+        public Municipio(String codigo, String territorio, Double valor) {
+            this.codigo = codigo;
+            this.territorio = territorio;
+            this.valor = valor;
+        }
+    }
+
+    public static void mediana(String archivoCsv) throws IOException {
+        System.out.println("El top 3 medianas de consumo por Municipios:");
+        // String archivoCsv = ".\\viviendas.csv";
+        List<String> lista = new ArrayList<>();
         try {
-            
+
             BufferedReader br = new BufferedReader(new FileReader(archivoCsv));
-            String linea=br.readLine();
-            while (linea != null) {
-                String[] fragmento=
+            String linea = br.readLine();
+            linea = br.readLine();
+            List<Municipio> listaValorMunicipios = new ArrayList<>();
+            while ((linea = br.readLine()) != null) {
+                String[] fragmento = linea.split(";", -1);
+                String codigo = fragmento[2] + " - ";
+                String territorio = fragmento[3] + " - ";
+                String valor = fragmento[4];
+
+                // System.out.println(codigo + territorio + valor);
+                if (!valor.isEmpty() && !valor.contains("-")) {
+                    double mediana = Double.parseDouble(valor);
+
+                    listaValorMunicipios.add(new Municipio(codigo, territorio, mediana));
+                    listaValorMunicipios.sort((a, b) -> Double.compare(a.valor, b.valor));
+
+                }
             }
+            int i = 0;
+            for (Municipio municipio : listaValorMunicipios.reversed()) {
+                if (i < 3) {
+                    System.out.println("codigo: " + municipio.codigo + "Territorio:  " + municipio.territorio + "valor: " + municipio.valor);
+
+                }
+                i++;
+
+            }
+
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-
 }
