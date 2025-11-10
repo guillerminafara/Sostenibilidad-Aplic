0f28834c884fe29a6f21b06d3b9595a1beb1b0e7
guillermina <faramagui@hotmail.com>
2025-11-10 12:26:30 +0100

    as

--- Unidad1/tarea4/tarea.php
+++ Unidad1/tarea4/tarea.php
@@ -9,7 +9,7 @@ $comunidades = [
 $calcularAcceso = function ($comunidad) {
     $accesoBase = $comunidad["acueducto"] ? 0.9 : 0.4; // 90% o 40% decobertura base
 
-    $bonoPozos = $comunidad["pozos"] / $comunidad["poblacion"] * 1000; //Bono por pozos (1 pozo cada 1000 personas = +10%)
+    $bonoPozos = ($comunidad["pozos"] / max(1,$comunidad["poblacion"]) )* 1000; //Bono por pozos (1 pozo cada 1000 personas = +10%)
     $toca = $bonoPozos > 1 ? 0.30 : 0; 
     return $accesoBase + $bonoPozos + $toca;
     // return $accesoBase + $bonoPozos ;
@@ -21,10 +21,13 @@ $poblacionConAcceso = 0;
 foreach ($comunidades as $comunidad) {
     $poblacionTotal += $comunidad["poblacion"];
     $acceso = $calcularAcceso($comunidad);
+    // $acceso = round($calcularAcceso($comunidad));
+
     $poblacionConAcceso += $comunidad["poblacion"] * $acceso;
-    echo "Población total $poblacionTotal, $acceso, con acceso? $poblacionConAcceso ";
-    echo "<br>";
 } 
+
+    echo "Población total $poblacionTotal, $acceso % , con acceso  $poblacionConAcceso ";
+    echo "<br>";
 
 echo "Cálculo completado";
 ?>
