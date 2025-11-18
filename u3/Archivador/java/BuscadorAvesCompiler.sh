af88b01387ee64fc70a60fb411887653429b783d
guillermina <faramagui@hotmail.com>
2025-11-18 07:55:07 +0100

    Sigue sin ir albuaves

--- u3/Archivador/java/BuscadorAvesCompiler.sh
+++ u3/Archivador/java/BuscadorAvesCompiler.sh
@@ -3,14 +3,15 @@
 # Compila
 echo " * Compilando el .java "
 # javac -cp .:json-20250517.jar SearchBirdsAPI.java
-javac -cp ".;json-20250517.jar" SearchBirdsAPI.java
+javac -cp ".;libs/json-20250517.jar" SearchBirdsAPI.java
 
 #javac -cp .:json-20250517.jar  -Xlint:deprecation SearchBirdsAPI.java
 
 # Ejecuta
 echo " * Ejecutando el Buscador "
 # /usr/lib/jvm/java-26-openjdk-amd64/bin/java -cp .:json-20250517.jar SearchBirdsAPI
-java -cp ".;json-20250517.jar;java" SearchBirdsAPI
+# java -cp ".;libs/json-20250517.jar" SearchBirdsAPI
+java -cp ".;libs/json-20250517.jar;java" SearchBirdsAPI
 
 
 
--- u3/Archivador/run-api-server.sh
+++ u3/Archivador/run-api-server.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 IPAPI="127.0.0.1"
 PORTAPI="9191"
-#  cd php/
+# cd ./php
 php -S ${IPAPI}:${PORTAPI} 
-
 exit 0
